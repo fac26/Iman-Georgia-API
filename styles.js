da68cc281b643d7642e0af5f6697b49b7819f893
ImanG <igeghellache@gmail.com>
2022-10-22 23:11:51 +0100

    adding marvel api index and js file updates

--- styles.js
+++ styles.js
@@ -1,34 +1,74 @@
-// you will also have to setup the referring domains on your marvel developer portal
-var PRIV_KEY = "74d9d52b69a9745e9972bfcc16060a71ae190f6b";
-var PUBLIC_KEY = "509766793c55d7eb72c977e1397d66f1";
+let count = 0;
+function heroinfo() {
 
-function getMarvelResponse() {
+    var marvelPublicKey = '509766793c55d7eb72c977e1397d66f1';
+    var marvelPrivateKey = '74d9d52b69a9745e9972bfcc16060a71ae190f6b';
 
-  // you need a new ts every request                                                                                    
-  var ts = new Date().getTime();
-  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
-  
-  // the api deals a lot in ids rather than just the strings you want to use
-  var characterId = '1009718'; // wolverine                                                                             
+    var name = document.getElementById('Name').value; //when button pressed ofc
 
+    var marvelApiStart = "https://gateway.marvel.com:443/v1/public/characters?apikey=";
 
-  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+    var ts = new Date().getTime();
+    var hash = md5(ts + marvelPrivateKey + marvelPublicKey);
+    var requestUrl = marvelApiStart + marvelPublicKey + "&ts=" + ts 
+        + "&hash=" + hash + "&nameStartsWith=" + name;
+    console.log(requestUrl);
 
-  console.log(url);
-  $.getJSON(url, {
-    ts: ts,
-    apikey: PUBLIC_KEY,
-    hash: hash,
-    characters: characterId
+    var result = fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
     })
-    .done(function(data) {
-      // sort of a long dump you will need to sort through
-      console.log(data);
-    })
-    .fail(function(err){
-      // the error codes are listed on the dev site
-      console.log(err);
+    .then(function (data) {
+        console.log(data);
     });
-};
 
-getMarvelResponse();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl);
+    xhr.send();
+
+    console.log('Loading...');
+    xhr.addEventListener('load', function() {
+
+    var data = JSON.parse(xhr.responseText);
+    var resultsLength = data.data.results;
+
+        var arr = {
+        nome: resultsLength[0].name,
+        description: resultsLength[0].description,
+        thumbnail: resultsLength[0].thumbnail.path.extension, //image
+        }
+
+        var seriesStr = '';
+        if (arr.series && arr.series.items) {
+        for (var x = 0; x < arr.series.items.length; x++) {
+            seriesStr += '<a href="#">' + arr.series.items[x].name + '</a>';
+        }
+        }
+
+        var hn = document.querySelector('#heroname'); //heroname
+        var hd = document.querySelector('#herodescription'); //herodescription
+
+        var nome = document.createElement('span');
+        var description = document.createElement('span');
+        var thumbnail = document.createElement('span'); //image
+
+        nome.innerHTML = arr.nome;
+        description.innerHTML = arr.description;
+        thumbnail.innerHTML = arr.thumbnail;
+        
+        hn.appendChild(nome);
+        hd.appendChild(description);
+        hd.appendChild(thumbnail); //to add image
+        count++;
+        //ok will probs use film API then for the pictures
+        //next steps: just implement search bar and add spaces between
+        console.log(count)
+    }
+    );
+}
+
+
+
+
+
+    
